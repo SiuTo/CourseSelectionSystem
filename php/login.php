fb6e7c4975cc8013fba26e91b43c8020b7dd926f
Xiaotao Liang <418xiaotao@163.com>
2015-05-02 20:48:41 +0800

    add the dropdown menu & modify the user system

--- php/login.php
+++ php/login.php
@@ -16,34 +16,47 @@
 	require "ConnectDB.php";
 
 	if ($userType=="student")
-	{
 		$result=mysql_query("SELECT SID, PASSWORD FROM STUDENT WHERE SID='$userId'");
-	}
 	else if ($userType=="teacher")
-	{
 		$result=mysql_query("SELECT TID, PASSWORD FROM TEACHER WHERE TID='$userId'");
-	}
 	else if ($userType=="admin")
-	{
 		$result=mysql_query("SELECT AID, PASSWORD FROM ADMIN WHERE AID='$userId'");
-	}
 	else {
 		echo "<script>alert('Illegal user type!'); history.go(-1);</script>";
 		exit;
 	}
 	$info=mysql_fetch_array($result);
-	if (empty($info))
+	
+	if ($_POST["submit"]=="signIn")
 	{
-		echo "<script>alert('User doesn\'t exist!'); history.go(-1);</script>";
-		exit;
-	}
-	if ($info['PASSWORD']!=$password)
+		if (empty($info))
+		{
+			echo "<script>alert('User doesn\'t exist!'); history.go(-1);</script>";
+			exit;
+		}
+		if ($info['PASSWORD']!=$password)
+		{
+			echo "<script>alert('Wrong user ID or password!'); history.go(-1);</script>";
+			exit;
+		}
+		session_start();
+		$_SESSION["userId"]=$userId;
+		$_SESSION["userType"]=$userType;
+		header("Location: ../$userType.php");
+	}else
 	{
-		echo "<script>alert('Wrong user ID or password!'); history.go(-1);</script>";
-		exit;
+		if (!empty($info))
+		{
+			echo "<script>alert('User exists!'); history.go(-1);</script>";
+			exit;
+		}
+		if ($userType=="student")
+			mysql_query("INSERT INTO STUDENT(SID, PASSWORD) VALUES ('$userId', '$password')");
+		else if ($userType=="teacher")
+			mysql_query("INSERT INTO STUDENT(TID, PASSWORD) VALUES ('$userId', '$password')");
+		else if ($userType=="admin")
+			mysql_query("INSERT INTO STUDENT(AID, PASSWORD) VALUES ('$userId', '$password')");
+		echo "<script>alert('Succeed!'); history.go(-1);</script>";
 	}
-	session_start();
-	$_SESSION["userId"]=$userId;
-	header("Location: ../$userType.php");
 ?>
 
