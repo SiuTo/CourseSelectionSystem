13f33a5a3f33e78bac27256280ee62a335a26340
Xiaotao Liang <418xiaotao@163.com>
2015-05-08 00:13:34 +0800

    add judgment of course conflict

--- php/student/addCourse.php
+++ php/student/addCourse.php
@@ -2,6 +2,21 @@
 	require "../verifyUser.php";
 	require "../ConnectDB.php";
 
+	function timeToInterval($str)
+	{
+		$p1=strpos($str, " ");
+		$p2=strpos($str, "-");
+		$day=substr($str, 0, $p1);
+		if ($day=="MON") $num=0;
+		else if ($day=="TUE") $num=1;
+		else if ($day=="WED") $num=2;
+		else if ($day=="THU") $num=3;
+		else if ($day=="FRI") $num=4;
+		else if ($day=="SAT") $num=5;
+		else if ($day=="SUN") $num=6;
+		return array($num*13+substr($str, $p1+1, $p2-$p1-1)-1, $num*13+substr($str, $p2+1)-1);
+	}
+
 	$courseId=$_POST["courseId"];
 	if ($_SESSION["userType"]=="admin") $sid=$_POST["sid"];
 	else $sid=$_SESSION["userId"];
@@ -28,6 +43,26 @@
 	{
 		echo "Fail:There is no vacancy for course $courseId!";
 		exit;
+	}
+
+	$result=mysql_query("SELECT TIME FROM SC, CSCHEDULE WHERE SID='$sid' AND SC.CID=CSCHEDULE.CID");
+	$flag=array_fill(0, 7*13, false);
+	while ($row=mysql_fetch_array($result))
+	{
+		$timeInterval=timeToInterval($row["TIME"]);
+		for ($i=$timeInterval[0]; $i<=$timeInterval[1]; ++$i) $flag[$i]=true;
+	}
+
+	$result=mysql_query("SELECT TIME FROM CSCHEDULE WHERE CID='$courseId'");
+	while ($row=mysql_fetch_array($result))
+	{
+		$timeInterval=timeToInterval($row["TIME"]);
+		for ($i=$timeInterval[0]; $i<=$timeInterval[1]; ++$i)
+			if ($flag[$i])
+			{
+				echo "Fail:There is time conflict for course $courseId!";
+				exit;
+			}
 	}
 
 	mysql_query("INSERT INTO SC(SID, CID) VALUES('$sid', '$courseId')");
